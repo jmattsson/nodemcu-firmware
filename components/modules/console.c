37dd459f12228bcfdb42ba54dcdd8e8be657bcc6
Jade Mattsson <jmattsson@dius.com.au>
2026-04-23 11:27:38 +1000

    Resolve usb serial jtag console deps issues

--- components/modules/console.c
+++ components/modules/console.c
@@ -7,6 +7,8 @@
 
 #include "esp_vfs_cdcacm.h"
 #include "driver/uart_vfs.h"
+#include "driver/usb_serial_jtag.h"
+#include "driver/usb_serial_jtag_vfs.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
