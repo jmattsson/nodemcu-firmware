e7f6815bd8bd15fd1d1a669cd051d60bed7340da
Jade Mattsson <jmattsson@dius.com.au>
2026-04-23 15:53:37 +1000

    Overhaul adc module for new IDF driver

--- components/modules/adc.c
+++ components/modules/adc.c
@@ -2,74 +2,218 @@
 
 #include "module.h"
 #include "lauxlib.h"
-#include "platform.h"
+
+#include "esp_adc/adc_oneshot.h"
+#include "hal/adc_types.h"
+
+#include <string.h>
+
+#define ADC_METATABLE "adc.instance"
+
+typedef struct {
+  adc_oneshot_unit_handle_t handle;
+} adc_ud_t;
+
+typedef enum { STATE_ALIVE, STATE_CLOSED_ACCEPTABLE } userdata_state_t;
 
 
-// Lua: config( adc_id, bits )
-static int adc_set_width( lua_State *L )
+// --- Helper functions --------------------------------------
+
+#define CASE_RETURN(x) case x: return x
+
+static adc_unit_t ladc_checkunit(lua_State *L, int idx)
 {
-  int adc_id = luaL_checkinteger( L, 1 );
-  MOD_CHECK_ID( adc, adc_id );
-  int bits = luaL_checkinteger( L, 2 );
-
-  if (!platform_adc_set_width( adc_id, bits ))
-    luaL_error( L, "adc_set_width failed" );
-
-  return 0;
+  int unit = luaL_checkinteger(L, idx);
+  switch(unit)
+  {
+    CASE_RETURN( ADC_UNIT_1 );
+    CASE_RETURN( ADC_UNIT_2 );
+    default: return luaL_error(L, "invalid ADC unit: %d", unit);
+  }
 }
 
-// Lua: setup( adc_id, channel, atten )
-static int adc_setup( lua_State *L )
+static adc_channel_t ladc_checkchannel(lua_State *L, int idx)
 {
-  int adc_id = luaL_checkinteger( L, 1 );
-  MOD_CHECK_ID( adc, adc_id );
-  int channel = luaL_checkinteger( L, 2 );
-  if (!platform_adc_channel_exists( adc_id, channel ))
-    luaL_error( L, "channel %d does not exist in ADC%d", ( unsigned )channel, ( unsigned )adc_id );
-
-  int atten = luaL_checkinteger( L, 3 );  
-
-  if (!platform_adc_setup( adc_id, channel, atten ))
-    luaL_error( L, "adc_setup failed" );
-
-  return 0;
+  int chan = luaL_checkinteger(L, idx);
+  switch(chan)
+  {
+    CASE_RETURN( ADC_CHANNEL_0 );
+    CASE_RETURN( ADC_CHANNEL_1 );
+    CASE_RETURN( ADC_CHANNEL_2 );
+    CASE_RETURN( ADC_CHANNEL_3 );
+    CASE_RETURN( ADC_CHANNEL_4 );
+    CASE_RETURN( ADC_CHANNEL_5 );
+    CASE_RETURN( ADC_CHANNEL_6 );
+    CASE_RETURN( ADC_CHANNEL_7 );
+    CASE_RETURN( ADC_CHANNEL_8 );
+    CASE_RETURN( ADC_CHANNEL_9 );
+    CASE_RETURN( ADC_CHANNEL_10);
+    default: return luaL_error(L, "invalid ADC channel: %d", chan);
+  }
 }
 
-// Lua: read( adc_id, channel )
-static int adc_read( lua_State *L )
+static adc_bitwidth_t ladc_checkoptbits(
+  lua_State *L, int idx, adc_bitwidth_t dflt)
 {
-  int adc_id = luaL_checkinteger( L, 1 );
-  MOD_CHECK_ID( adc, adc_id );
-  int channel = luaL_checkinteger( L, 2 );
-  if (!platform_adc_channel_exists( adc_id, channel ))
-    luaL_error( L, "channel %d does not exist in ADC%d", ( unsigned )channel, ( unsigned )adc_id );
+  if (lua_isnoneornil(L, idx))
+    return dflt;
+  int bits = luaL_checkinteger(L, idx);
+  switch(bits)
+  {
+    CASE_RETURN( ADC_BITWIDTH_9  );
+    CASE_RETURN( ADC_BITWIDTH_10 );
+    CASE_RETURN( ADC_BITWIDTH_11 );
+    CASE_RETURN( ADC_BITWIDTH_12 );
+    CASE_RETURN( ADC_BITWIDTH_13 );
+    default: return luaL_error(L, "invalid ADC bits: %d", bits);
+  }
+}
 
-  int sample = platform_adc_read( adc_id, channel );
-  if (sample == -1)
-    luaL_error( L, "adc_read failed" );
-  lua_pushinteger( L, ( lua_Integer ) sample );
+static adc_atten_t ladc_checkoptatten(lua_State *L, int idx, adc_atten_t dflt)
+{
+  if (lua_isnoneornil(L, idx))
+    return dflt;
+  int atten = luaL_checkinteger(L, idx);
+  switch(atten)
+  {
+    CASE_RETURN( ADC_ATTEN_DB_0 );
+    CASE_RETURN( ADC_ATTEN_DB_2_5 );
+    CASE_RETURN( ADC_ATTEN_DB_6 );
+    CASE_RETURN( ADC_ATTEN_DB_12 );
+    default: return luaL_error(L, "invalid ADC attenuation: %d", atten);
+  }
+}
+
+static void ladc_checkerr(lua_State *L, esp_err_t err)
+{
+  switch(err)
+  {
+    case ESP_OK: return;
+    case ESP_ERR_NOT_FOUND: luaL_error(L, "ADC already in use"); return;
+    default: luaL_error(L, "ADC initialisation error: %d", err);
+  }
+}
+
+static adc_ud_t *ladc_checkuserdata(lua_State *L, int idx, userdata_state_t s)
+{
+  adc_ud_t *ud = (adc_ud_t *)luaL_checkudata(L, idx, ADC_METATABLE);
+  if (s == STATE_ALIVE && ud->handle == NULL)
+    luaL_error(L, "ADC instance closed");
+  return ud;
+}
+
+
+// --- Lua API implementation ---------------------------------
+
+// Lua: adc.setup(unit)
+static int ladc_setup(lua_State *L)
+{
+  // Sanity check arguments
+  adc_unit_t unit = ladc_checkunit(L, 1);
+
+  // Create the user data object
+  adc_ud_t *ud = (adc_ud_t *)lua_newuserdata(L, sizeof(adc_ud_t));
+  luaL_getmetatable(L, ADC_METATABLE);
+  lua_setmetatable(L, -2);
+  memset(ud, 0, sizeof(*ud)); // ensure error path doesn't see random data
+
+  // Create ADC unit handle
+  adc_oneshot_unit_init_cfg_t unit_cfg = {
+    .unit_id = unit,
+    .clk_src = 0, // use default
+    .ulp_mode = ADC_ULP_MODE_DISABLE,
+  };
+  ladc_checkerr(L, adc_oneshot_new_unit(&unit_cfg, &ud->handle));
+
+  // Returning userdata object
   return 1;
 }
 
-// Lua: read_hall_sensor( )
-static int read_hall_sensor( lua_State *L )
+
+// Lua: adcObj:close() -- or garbage collection
+static int ladc_close(lua_State *L)
 {
-  const char *msg = "ADC hall sensor no longer supported in IDF, sorry";
-  platform_print_deprecation_note("msg", "IDFv5");
-  return luaL_error(L, msg);
+  adc_ud_t *ud = ladc_checkuserdata(L, 1, STATE_CLOSED_ACCEPTABLE);
+  // On GC, may already be closed, so have to check here
+  if (ud->handle)
+  {
+    adc_oneshot_del_unit(ud->handle);
+    ud->handle = NULL;
+  }
+
+  return 0;
 }
 
+// Lua: adcObj:configure(channel, bits, atten)
+static int ladc_configure(lua_State *L)
+{
+  adc_ud_t *ud = ladc_checkuserdata(L, 1, STATE_ALIVE);
+  adc_channel_t channel = ladc_checkchannel(L, 2);
+  adc_bitwidth_t bits = ladc_checkoptbits(L, 3, ADC_BITWIDTH_DEFAULT);
+  adc_atten_t atten = ladc_checkoptatten(L, 4, ADC_ATTEN_DB_0);
+
+  adc_oneshot_chan_cfg_t cfg = {
+    .atten = atten,
+    .bitwidth = bits,
+  };
+  ladc_checkerr(L, adc_oneshot_config_channel(ud->handle, channel, &cfg));
+
+  return 0;
+}
+
+// Lua: adcObj:read(channel)
+static int ladc_read(lua_State *L)
+{
+  adc_ud_t *ud = ladc_checkuserdata(L, 1, STATE_ALIVE);
+  adc_channel_t channel = ladc_checkchannel(L, 2);
+
+  int raw = 0;
+  ladc_checkerr(L, adc_oneshot_read(ud->handle, channel, &raw));
+
+  lua_pushinteger(L, raw);
+  return 1;
+}
+
+
 // Module function map
+LROT_BEGIN(adc_dyn, NULL, LROT_MASK_GC_INDEX)
+  LROT_FUNCENTRY( __gc,             ladc_close )
+  LROT_TABENTRY ( __index,          adc_dyn )
+  LROT_FUNCENTRY( close,            ladc_close)
+  LROT_FUNCENTRY( configure,        ladc_configure )
+  LROT_FUNCENTRY( read,             ladc_read)
+LROT_END(adc_dyn, NULL, LROT_MASK_GC_INDEX)
+
 LROT_BEGIN(adc, NULL, 0)
-  LROT_FUNCENTRY( setwidth,         adc_set_width )
-  LROT_FUNCENTRY( setup,            adc_setup )
-  LROT_FUNCENTRY( read,             adc_read )
-  LROT_FUNCENTRY( read_hall_sensor, read_hall_sensor )
-  LROT_NUMENTRY ( ATTEN_0db,        PLATFORM_ADC_ATTEN_0db )
-  LROT_NUMENTRY ( ATTEN_2_5db,      PLATFORM_ADC_ATTEN_2_5db )
-  LROT_NUMENTRY ( ATTEN_6db,        PLATFORM_ADC_ATTEN_6db )
-  LROT_NUMENTRY ( ATTEN_11db,       PLATFORM_ADC_ATTEN_11db )
-  LROT_NUMENTRY ( ADC1,             1 )
+  LROT_FUNCENTRY( setup,            ladc_setup )
+  LROT_NUMENTRY ( ADC1,             ADC_UNIT_1 )
+  LROT_NUMENTRY ( ADC2,             ADC_UNIT_2 )
+  LROT_NUMENTRY ( ATTEN_0,          ADC_ATTEN_DB_0 )
+  LROT_NUMENTRY ( ATTEN_2_5,        ADC_ATTEN_DB_2_5 )
+  LROT_NUMENTRY ( ATTEN_6,          ADC_ATTEN_DB_6 )
+  LROT_NUMENTRY ( ATTEN_12,         ADC_ATTEN_DB_12 )
+  LROT_NUMENTRY ( BITS_9,           ADC_BITWIDTH_9 )
+  LROT_NUMENTRY ( BITS_10,          ADC_BITWIDTH_10 )
+  LROT_NUMENTRY ( BITS_11,          ADC_BITWIDTH_11 )
+  LROT_NUMENTRY ( BITS_12,          ADC_BITWIDTH_12 )
+  LROT_NUMENTRY ( BITS_13,          ADC_BITWIDTH_13 )
+  LROT_NUMENTRY ( CHANNEL_0,        ADC_CHANNEL_0 )
+  LROT_NUMENTRY ( CHANNEL_1,        ADC_CHANNEL_1 )
+  LROT_NUMENTRY ( CHANNEL_2,        ADC_CHANNEL_2 )
+  LROT_NUMENTRY ( CHANNEL_3,        ADC_CHANNEL_3 )
+  LROT_NUMENTRY ( CHANNEL_4,        ADC_CHANNEL_4 )
+  LROT_NUMENTRY ( CHANNEL_5,        ADC_CHANNEL_5 )
+  LROT_NUMENTRY ( CHANNEL_6,        ADC_CHANNEL_6 )
+  LROT_NUMENTRY ( CHANNEL_7,        ADC_CHANNEL_7 )
+  LROT_NUMENTRY ( CHANNEL_8,        ADC_CHANNEL_8 )
+  LROT_NUMENTRY ( CHANNEL_9,        ADC_CHANNEL_9 )
+  LROT_NUMENTRY ( CHANNEL_10,       ADC_CHANNEL_10 )
 LROT_END(adc, NULL, 0)
 
-NODEMCU_MODULE(ADC, "adc", adc, NULL);
+static int ladc_init(lua_State *L)
+{
+  luaL_rometatable(L, ADC_METATABLE, LROT_TABLEREF(adc_dyn));
+  return 0;
+}
+
+NODEMCU_MODULE(ADC, "adc", adc, ladc_init);
--- docs/modules/adc.md
+++ docs/modules/adc.md
@@ -1,62 +1,134 @@
 # ADC Module
 | Since  | Origin / Contributor  | Maintainer  | Source  |
 | :----- | :-------------------- | :---------- | :------ |
-| 2017-04-22 | [zelll](https://github.com/zelll) | [zelll](https://github.com/zelll) | [adc.c](../../components/modules/adc.c)|
+| 2017-04-22 | [zelll](https://github.com/zelll) | [jmattsson](https://github.com/jmattsson) | [adc.c](../../components/modules/adc.c)|
 
-The ADC module provides access to the in-built ADC1.
+The ADC module provides access to the in-built ADC hardware.
 
-On the ESP32 there are two ADC. ADC1 has 8 channels, while ADC2 has 10 channels. Currently, only ADC1 is supported.
+Due to the original Espressif ADC driver being removed in ESP IDF 6.0, this
+module had to be rewritten in a not backwards compatible manner. Also, the
+old module was written for only the ESP32, and the series has grown
+substantially since then.
 
-## adc.setwidth()
+Instead of the old behaviour which was locked to ADC1, the current `adc`
+module offers more flexibility. On chips where ADC2 is available, it too
+may be used.
 
-The configuration is in effect for all channels of ADC1
+To begin, first claim the ADC hardware unit by calling `adc.setup(adc.ADC1)`
+(or `adc.ADC2`). This returns an adc instance which can then be configured and
+read from. When no further ADC readings are required, the instance can
+either be manually closed or left to be garbage collected, at which point
+it will be closed automatically.
 
-#### Syntax
-`adc.setwidth(adc_number, bits)`
-
-#### Parameters
-- `adc_number` Only `adc.ADC1` now
-- `bits` One of `9`/`10`/`11`/`12`.
-
-#### Returns
-`nil`
-
+Quick example:
+```lua
+myadc = adc.setup(adc.ADC1)
+myadc:configure(adc.CHANNEL_0, adc.BITS_9, adc.ATTEN_6)
+val1 = myadc:read(adc.CHANNEL_0)
+-- later
+val2 = myadc:read(adc.CHANNEL_0)
+-- when done (if ever)
+myadc:close()
+```
 
 ## adc.setup()
 
-Configuration ADC1 capture attenuation of channels
+Claim the hardware ADC unit and initialise it.
 
 #### Syntax
-`adc.setup(adc_number, channel, atten)`
+`adc.setup(adc_unit`)
 
 #### Parameters
-- `adc_number` Only `adc.ADC1` now
-- `channel`  When using `adc.ADC1`: `0` to `7`. 0: GPIO36, 1: GPIO37, 2: GPIO38, 3: GPIO39, 4: GPIO32, 5: GPIO33, 6: GPIO34, 7: GPIO35
-- `atten` One of following constants
-	- `adc.ATTEN_0db`    The input voltage of ADC will be reduced to about 1/1    (1.1V when VDD_A=3.3V)
-	- `adc.ATTEN_2_5db`  The input voltage of ADC will be reduced to about 1/1.34 (1.5V when VDD_A=3.3V)
-	- `adc.ATTEN_6db`    The input voltage of ADC will be reduced to about 1/2    (2.2V when VDD_A=3.3V)
-	- `adc.ATTEN_11db`   The input voltage of ADC will be reduced to about 1/3.6  (3.9V when VDD_A=3.3V,  maximum voltage is limited by VDD_A)
+- `adc_unit` One of `adc.ADC1` or `adc.ADC2` (chip/module dependent).
+
+#### Returns
+An adc instance object. If the ADC has already been claimed, or cannot be
+initialised for another reason, then an error will be raised.
+
+#### Example
+```lua
+myadc = adc.setup(adc.ADC2)
+```
+
+## adc.obj:configure()
+
+ADC channel configuration on a set up adc instance.
+
+#### Syntax
+`myadc:configure(adc_channel, bits, attenuation)`
+
+#### Parameters
+- `adc_channel` One of `adc.CHANNEL_0` through to `adc.CHANNEL_10` (chip/module dependent). Check the datasheet for what channel corresponds to what pin.
+- `bits` The bit width for the ADC, one of `adc.BITS_9` through to `adc.BITS_13`, or `nil` to use the default (maximum) the hardware supports.
+- `attenuation` Signal attenuation, one of:
+  - `adc.ATTEN_0` for no attentuation
+  - `adc.ATTEN_2_5` for 2.5dB attenuation (reduce signal by ~25%)
+  - `adc.ATTEN_6` for 6dB attenuation (reduce signal by ~50%)
+  - `adc.ATTEN_12` for 12dB attenuation (reduce signal by ~75%)
+  - `nil` for the default (no attenuation)
 
 #### Returns
 `nil`
 
+#### Examples
+Default configuration of channel 0:
+```lua
+myadc:configure(adc.CHANNEL_0)
+```
 
-## adc.read()
+Bitwidth configuration on channel 2, with no attenuation:
+```lua
+myadc:configure(adc.CHANNEL_2, adc.BITS_12)
+```
 
-Samples the ADC. You should to call `setwidth()` before `read()`.
+Attenuation on channel 5, with default bit width.
+```lua
+myadc:configure(adc.CHANNEL_5, nil, adc.ATTEN_2_5)
+```
+
+Custom bit width and attentuation of channel 8:
+```lua
+myadc:configure(adc.CHANNEL_8, adc.BITS_9, adc.ATTEN_12)
+```
+
+
+## adc.obj:read()
+
+Obtains a reading of the ADC. Prior to reading, ensure the channel has been
+configured as needed.
 
 #### Syntax
-`adc.read(adc_number, channel)`
+`myadc:read(adc_channel)`
 
 #### Parameters
-- `adc_number` Only `adc.ADC1` now
-- `channel` 0 to 7 for adc.ADC1
+- `adc_channel` One of `adc.CHANNEL_0` through to `adc.CHANNEL_10` (chip/module dependent). Check the datasheet for what channel corresponds to what pin.
 
 #### Returns
-the sampled value (number)
+The raw ADC value, as an integer. An error is raised in case of failure.
 
 #### Example
 ```lua
-val = adc.read(adc.ADC1, 0)
+val = myadc:read(adc.CHANNEL_0)
 ```
+
+
+## adc.obj:close()
+
+Closes the adc instance and releases the underlying hardware. No further
+reading or configuring can be done on this object after this. To use the
+ADC again, a new instance must be obtained via `adc.setup()`.
+
+It is optional to call this function. If the adc instance gets garbage
+collected, it will be automatically closed then.
+
+A common, valid usage scenario sees the ADC configured at start and then never
+closed/released.
+
+#### Syntax
+`myadc:close()`
+
+#### Parameters
+None.
+
+#### Returns
+`nil`
